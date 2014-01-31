d1ca21741215a17cb94e27a3ae8ea3326b918f4c
dimanne <dimanne@ya.ru>
2014-01-31 12:11:40 +0400

    Added trulee info table

--- firewall/TRulesInfoTable.h
+++ firewall/TRulesInfoTable.h
@@ -1,4 +1,26 @@
 #ifndef FIREWALL_TRULESINFOTABLE_H
 #define FIREWALL_TRULESINFOTABLE_H
 
+#include "TProtocolsInfoTable.h"
+
+enum class TAction { Accept, Drop, Log, LAST};
+
+typedef QStringList TSubjects;
+
+typedef QHash<TAction, TSubjects> TSubjectsInfo;
+
+typedef QStringList TProcessNames;
+typedef QStringList TUserNames;
+
+struct TMetaRuleInfo
+   {
+      TProtocolNames ProtocolNames;
+      TSubjectsInfo InputRules;
+      TSubjectsInfo OutputRules;
+      TProcessNames ProcessNames;
+      TUserNames UserNames;
+   };
+
+typedef QVector<TMetaRuleInfo> TMetaRuleInfos;
+
 #endif // FIREWALL_TRULESINFOTABLE_H
